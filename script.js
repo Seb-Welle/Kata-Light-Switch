700f9ebe59e644053f7e14248a0e34f3a831ea53
Sebastian Welle <s-welle@web.de>
2022-03-01 10:50:40 +0100

    fix links between files and adds styles

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ btn.addEventListener("click", () => {
   const body = document.body;
   if (body.style.backgroundColor === "black") {
     body.style.backgroundColor = "white";
+    btn.classList.toggle("button-dark");
+    document.title = "Good Night";
   } else {
     body.style.backgroundColor = "black";
+    btn.classList.toggle("button-dark");
+    document.title = "Good Morning";
   }
 });
